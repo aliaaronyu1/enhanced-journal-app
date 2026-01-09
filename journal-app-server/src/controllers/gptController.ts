83bde0314120259369364f215ab58d8c91bf604e
aliaaronyu1 <agyu@dons.usfca.edu>
2026-01-08 17:28:11 -0800

    removed comments

--- journal-app-server/src/controllers/gptController.ts
+++ journal-app-server/src/controllers/gptController.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express";
 import { openai, SYSTEM_PROMPT } from "../lib/openai"
 import { createMessageService, getMessageHistoryService, getOrCreateConversationService, getLatestEntrySnapshotService, createEntrySnapshotService } from "../services/conversationServices";
-import { getEntryById } from "../services/journalServices";
 
 export const callGPT = async (req: Request, res: Response) => {
     const { entry, mood } = await req.body;
@@ -29,52 +28,6 @@ export const callGPT = async (req: Request, res: Response) => {
     })
 
 }
-
-// export const createMessage = async (req: Request, res: Response) => {
-//     const { user_id, entry_id } = req.params;
-//     const { role, content, isResubmitEntry } = req.body;
-
-//     //call our service that will either create a conversation or return the existing one depending on if it was created or not
-//     const conversation = await getOrCreateConversationService(entry_id)
-
-//     //get existing conversation messages
-//     const history = await getMessageHistoryService(conversation.id)
-
-//     //Create the message in journal_ai_messages
-//     const userMessage = await createMessageService(role, content, conversation.id)
-
-//     //Prepare AI context
-//     let messagesForAI = [
-//         { role: "system", content: SYSTEM_PROMPT }
-//     ];
-
-//     if (isResubmitEntry) {//swap a different condition. If this is a resubmission of an entry, we run this code
-//         messagesForAI.push({
-//             role: "entry",
-//             content: `Here is my journal entry resubmission: "${content}"`
-//         });
-//         //Store entry as well as a message with it's own role. It needs it's own role to keep track of entry versions
-//         const storeEntry = await createMessageService("entry", content, conversation.id)
-//     } else {
-//         // Standard flow: add history + new message
-//         const formattedHistory = history.map(m => ({ role: m.role, content: m.content }));
-//         messagesForAI = [...messagesForAI, ...formattedHistory, { role, content }];
-//     }
-
-
-//     const aiResponse = await openai.responses.create({
-//         model: "gpt-5-mini",
-//         input: messagesForAI as any
-//     });
-
-//     // aiResponse.output_text <-- create a message in DB for this
-//     const aiMessage = await createMessageService('system', aiResponse.output_text, conversation.id)
-
-//     res.status(200).json({
-//         userMessage,
-//         aiResponse
-//     })
-// }
 
 export const createMessage = async (req: Request, res: Response) => {
     const { entry_id } = req.params;
@@ -126,37 +79,9 @@ export const getMessages = async (req: Request, res: Response) => {
     const { user_id, entry_id } = req.params;
 
     try {
-        //parameters: entryId
         const conversation = await getOrCreateConversationService(entry_id)
 
-        //parameters: conversationId
-        //get all messages with that conversation id
-        //get existing conversation messages
         let messages = await getMessageHistoryService(conversation.id)
-
-        // //if there are no messages yet, flag that this is the first message
-        // let isFirstMessage = messages.length === 0;
-
-        // if (isFirstMessage) {//we want to submit the entry to AI if this is the first message
-        //     //Prepare AI context
-        //     let messagesForAI = [
-        //         { role: "system", content: SYSTEM_PROMPT }
-        //     ];
-
-        //     const journalEntry = await getEntryById(user_id, entry_id) //Why do we need this? can't we just pass the entry as 'content' from the frontend?
-        //     messagesForAI.push({
-        //         role: "entry",
-        //         content: `Here is my journal entry: "${journalEntry.body}"`
-        //     });
-        //     //Store entry as well as a message with it's own role. It needs it's own role to keep track of entry versions
-        //     const storeEntry = await createMessageService("entry", journalEntry.body, conversation.id)
-
-        //     const aiResponse = await openai.responses.create({
-        //         model: "gpt-5-mini",
-        //         input: messagesForAI as any
-        //     });
-        //     messages = await getMessageHistoryService(conversation.id)
-        // }
 
         res.status(200).json({
             conversationId: conversation.id,
@@ -211,14 +136,3 @@ export const submitEntry = async (req: Request, res: Response) => {
       res.status(500).json({ error: "Failed to submit entry" });
     }
   }
-
-
-//deleteConversation <-- if they delete the entry or just delete the conversation all together and want to start over
-
-
-
-//deleteMessages <-- Will this ever happen?
-
-//updateMessage <-- do I want to support this?
-
-//updateConversation <-- don't think we need to do this

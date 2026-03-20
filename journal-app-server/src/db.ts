f2ee12d04e49f5d27de5890312805bbc8ad81e02
aliaaronyu1 <agyu@dons.usfca.edu>
2026-03-19 18:51:23 -0700

    updated db.ts for neon db

--- journal-app-server/src/db.ts
+++ journal-app-server/src/db.ts
@@ -1,9 +1,8 @@
 import { Pool } from "pg";
 
 export const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "local_journal_DB",
-  password: "ilovepizza",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
